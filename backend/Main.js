19b0dd97c5bee9909f73ae218cfe1ff97adcf16b
Avi <avishiek@gmail.com>
2022-08-31 23:31:10 -0400

    first attempt at get route

--- backend/Main.js
+++ backend/Main.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// app.get('/', (req,res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+app.get('/api/contacts', (req,res) => {
+    const contacts = [
+        {name: 'John Doe', phone: 4584624623, email: 'john.doe@gmail.com'}
+    ];
+    res.json(contacts);
 });
 
-app.listen(3000);
+const port = 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
